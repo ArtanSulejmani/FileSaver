be2df432acb804283707267cc25f9b9615c25e99
Artan Sulejmani <artan-sulejmani@live.com>
2022-10-10 14:18:53 +0200

    Add project files.

---
+++ FileSaver/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.SFDWriter = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(201, 23)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(381, 284)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Save
+        '
+        Me.Save.Location = New System.Drawing.Point(315, 324)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(150, 37)
+        Me.Save.TabIndex = 1
+        Me.Save.Text = "Save"
+        Me.Save.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(801, 451)
+        Me.Controls.Add(Me.Save)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Calibri", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "TextSaver"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Save As Button
+    Friend WithEvents SFDWriter As SaveFileDialog
+End Class
---
+++ FileSaver/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim SW As StreamWriter
+
+    Private Sub Save_Click(sender As Object, e As EventArgs) Handles Save.Click
+        SFDWriter.ShowDialog()
+    End Sub
+
+    Private Sub SFDWriter_FileOk(
+            sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SFDWriter.FileOk
+        If SFDWriter.FileName <> "" Then
+
+        End If
+        SW = New StreamWriter(SFDWriter.FileName, False)
+
+        For Each line As String In Split(TextBox1.Text, Chr(13))
+            SW.WriteLine(line)
+
+        Next
+        SW.Close()
+
+
+    End Sub
+End Class
